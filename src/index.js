e94a770cbd8528b81714dabb9cec73cb10c65789
Valeria Osorio <valeriaosorio@Valerias-MacBook-Pro.local>
2024-01-14 12:24:04 -0500

    Adds normalize stylesheet

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import 'normalize.css'
 import './index.css'
 import Home from './pages/Home'
 import Dashboard from './pages/Dashboard'
