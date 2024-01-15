50526c2272e9abbc1482da866e37f318671f595b
Valeria Osorio <valeriaosorio@Valerias-MacBook-Pro.local>
2024-01-15 17:20:36 -0500

    Adds graphics library and renders precipitation graph in component

--- src/components/PrecipitationGraph/index.jsx
+++ src/components/PrecipitationGraph/index.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
 import axios from 'axios'
+import { ComposedChart, Line, Area, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts'
 import './PrecipitationGraph.css'
 
 const PrecipitationGraph = ({ location }) => {
@@ -13,29 +14,50 @@ const PrecipitationGraph = ({ location }) => {
       const data = response.data
       const list = data.list
 
-      const precipitationPosibility = list.map((el) => {
+      const newObj = list.map((el) => {
         return {
-          precipitationPosibility: el.pop * 100,
-          date: list.dt
-        }
-      })
-
-      const temperature = list.map((el) => {
-        return {
+          // Posibility of precipitation. API returns a number between 0 and 1 so we must multiply for 100 to get a percentage value
+          precipitationProbability: el.pop * 100,
+          // Temperature. API returns value in celsius after passing query param 'units=metric'
           temperature: el.main.temp,
-          date: list.dt
+          // Date. API returns date in 'yyyy-mm-dd hh:mm:ss"
+          Date: el.dt_txt
         }
       })
-
-      const newObj = { precipitationPosibility, temperature }
 
       setPrecipitationData(newObj)
-
-      console.log(precipitationData)
-      console.log(newObj)
     })
   }, [])
-  return <div className='precipitation-graph'>{location.lat}</div>
+
+  return (
+    <>
+      <div className='precipitation-graph'>
+        <div className='precipitation-graph__container'>
+          <ResponsiveContainer width='100%' height='100%'>
+            <ComposedChart
+              width={500}
+              height={400}
+              data={precipitationData}
+              margin={{
+                top: 8,
+                right: 8,
+                bottom: 8,
+                left: 8
+              }}
+            >
+              <CartesianGrid stroke='#f5f5f5' />
+              <XAxis dataKey='Date' scale='band' />
+              <YAxis />
+              <Tooltip />
+              <Legend />
+              <Area type='monotone' dataKey='precipitationProbability' fill='#9a8c98' stroke='#8884d8' />
+              <Line type='monotone' dataKey='temperature' stroke='#74A4BC' />
+            </ComposedChart>
+          </ResponsiveContainer>
+        </div>
+      </div>
+    </>
+  )
 }
 
 PrecipitationGraph.propTypes = { location: PropTypes.isRequired }
